fb794c2a83df50873aee727b5edb8fe56250a0a7
boy397 <aryamukherjee397@outlook.com>
2026-02-23 20:31:09 +0530

    changes

--- src/server.ts
+++
@@ -1,8 +0,0 @@
-import app from "./app"
-import { env } from "./config/env"
-
-const PORT = env.PORT || 5000
-
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`)
-})
